0f8515ccfc05487fea2b4c5cefcbb3252debeff5
henryofjupiter <oshardhenry@gmail.com>
2026-09-20 20:21:54 -0500

    add user input functionality, and refactors code

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // import {fetchWeatherApi} from "openmeteo";
 
 const apiKey = '3db8f23e418543c297c215245262009';
-const apiUrl = 'http://api.weatherapi.com/v1/current.json?';
-let country = 'London';
+const apiUrl = 'http://api.weatherapi.com/v1/current.json';
+let count = 'London';
 
-async function checkWeather() {
-    const response = await fetch(apiUrl  + `&q=${country}` + `&key=${apiKey}`);
-    return await response.json();
-   
-}
-checkWeather().then((data) => {
+const input = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+async function checkWeather(country) {
+    const response = await fetch(apiUrl  + '?&q='+ `${country}` + `&key=${apiKey}`);
+    const data = await response.json();
+    
     console.log(data);
-        document.querySelector(".city").innerHTML = data.location.name;
-        document.querySelector(".temp").innerHTML = data.current.temp_f + '&degc';
-        document.querySelector(".humidity").innerHTML = data.current.humidity + '%';
-        document.querySelector(".wind").innerHTML = data.current.wind_kph + ' km/h';
+    document.querySelector(".city").innerHTML = data.location.name;
+    document.querySelector(".temp").innerHTML = data.current.temp_f + '&deg';
+    document.querySelector(".humidity").innerHTML = data.current.humidity + '%';
+    document.querySelector(".wind").innerHTML = data.current.wind_kph + ' km/h';
+    
+    
+}
 
-}).catch((error) => {
-    console.error('error:', error);
-});
+searchBtn.addEventListener('click', () => {
+    const userInput = input.value.trim();
+    if (userInput){
+       checkWeather(userInput).then(r => true); 
+    }
+})
