2487acef31ccb9ce9b6bd49e21d807d472f0ca13
henryofjupiter <oshardhenry@gmail.com>
2026-09-23 15:34:29 -0500

    add dayGradient function for changing gradient when its day or night. add render function to handle rendering data to the html does minor refactoring of code

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const apiKey = '3db8f23e418543c297c215245262009';
 const apiUrl = 'https://api.weatherapi.com/v1/current.json';
 const input = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
+
+//checks if day--> then change gradients of card & background
+const dayGradient = (data) => {
+    const dayG = document.querySelector('.card');
+    const dayGB = document.querySelector('.changer');
+    if ((data.current.is_day) === 1) {
+        dayG.style.background = 'linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%)';
+        dayGB.style.background = '#013e50';
+    }
+    else {
+        dayG.style.background = 'linear-gradient(10deg, rgba(19, 0, 36, 1) 17%, rgba(28, 28, 176, 1) 55%, rgba(97, 2, 207, 1) 86%)';
+        dayGB.style.background = '#211442';
+    }
+}
+
 
 const skyCondition = (data) => {
+    const weatherIcon = document.querySelector('.weather-icon');
+    
     if ((data.current.cloud) >= 90) {
         if ((data.current.is_day) === 0) {
             if ((data.current.condition.text.includes('rain'))) {
@@ -67,18 +83,31 @@ const skyCondition = (data) => {
     }
 }
 
-
-async function checkWeather(country) {
-    const response = await fetch(apiUrl + '?&q=' + `${country}` + `&key=${apiKey}`);
-    const data = await response.json();
-
+//render 
+const render = (data) => {
     console.log(data);
     document.querySelector(".city").innerHTML = data.location.name;
     document.querySelector(".temp").innerHTML = data.current.temp_f + '&deg';
     document.querySelector(".humidity").innerHTML = data.current.humidity + '%';
     document.querySelector(".wind").innerHTML = data.current.wind_kph + ' km/h';
+    if (data.current.condition.text.includes('clear')){
+        document.querySelector(".conditions").innerHTML = data.current.condition.text + 'skies';
+    }
+    else {
+        document.querySelector(".conditions").innerHTML = data.current.condition.text;
+    }
+   
+}
 
+// fetch data from api
+async function checkWeather(country) {
+    const response = await fetch(apiUrl + '?&q=' + `${country}` + `&key=${apiKey}`);
+    const data = await response.json();
+    
+    //renders data to html-> displays sky conditions
+    render(data);
     skyCondition(data);
+    dayGradient(data);
 }
 
 searchBtn.addEventListener('click', () => {
