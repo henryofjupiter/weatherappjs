5257d2673e1549e3da92df75121ea2b9d282a52a
henryofjupiter <oshardhenry@gmail.com>
2026-09-25 14:50:06 -0500

    dynamically switch between homescreen on load, and weather details when searched

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const apiKey = '3db8f23e418543c297c215245262009';
 const apiUrl = 'https://api.weatherapi.com/v1/current.json';
 const input = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const homepage = document.querySelector('.weather');
 
 //checks if day--> then change gradients of card & background
 const dayGradient = (data) => {
     const dayG = document.querySelector('.card');
     const dayGB = document.querySelector('.changer');
-    
+
     if ((data.current.is_day) === 1) {
         dayG.style.background = 'linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%)';
         dayGB.style.background = '#054845';
-    }
-    else {
+    } else {
         dayG.style.background = 'linear-gradient(10deg, rgba(19, 0, 36, 1) 17%, rgba(28, 28, 176, 1) 55%, rgba(97, 2, 207, 1) 86%)';
         dayGB.style.background = '#12072f';
     }
@@ -21,7 +21,7 @@ const dayGradient = (data) => {
 
 const skyCondition = (data) => {
     const weatherIcon = document.querySelector('.weather-icon');
-    
+
     if ((data.current.cloud) >= 90) {
         if ((data.current.is_day) === 0) {
             if ((data.current.condition.text.includes('rain'))) {
@@ -91,33 +91,42 @@ const render = (data) => {
     document.querySelector(".temp").innerHTML = data.current.temp_f + '&deg';
     document.querySelector(".humidity").innerHTML = data.current.humidity + '%';
     document.querySelector(".wind").innerHTML = data.current.wind_kph + ' km/h';
-    document.querySelector(".willrain").innerHTML = data.current.chance_of_rain + '% change of\ rain';
-    
+    document.querySelector(".willrain").innerHTML = data.current.chance_of_rain + '% change of rain';
+
     //output weather condition
-    if (data.current.condition.text.includes('Clear') || ((data.current.condition.text.includes('Cloudy')))){
+    if (data.current.condition.text.includes('Clear') || ((data.current.condition.text.includes('Cloudy')))) {
         document.querySelector(".conditions").innerHTML = data.current.condition.text + ' skies';
-    }
-    else {
+    } else {
         // fetch weather conditions
         document.querySelector('.conditions').innerHTML = data.current.condition.text;
     }
-    
+
 }
 
 // fetch data from api
 async function checkWeather(country) {
     const response = await fetch(apiUrl + '?&q=' + `${country}` + `&key=${apiKey}`);
     const data = await response.json();
-    
+
+    const homepage2 = document.getElementById('homepage2');
+
+    if (data.location.name) {
+        console.log('at block');
+        homepage2.style.display = 'none';
+        homepage.classList.remove('weather');
+    }
     //renders data to html-> displays sky conditions
     render(data);
     skyCondition(data);
     dayGradient(data);
 }
 
+
+
 searchBtn.addEventListener('click', () => {
     const userInput = input.value.trim();
-    if (userInput) {
-        checkWeather(userInput).then(r => true);
-    }
-})
+    checkWeather(userInput).then(r => true);
+
+});
+
+
